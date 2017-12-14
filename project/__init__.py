0cb5ef5f9ab25d8f5f5dc855eaa30e982d24729a
Benjamin Wallad <Benjamin@MacBook-Pro.local>
2017-12-13 20:58:50 -0500

    Dockerize

--- project/__init__.py
+++ project/__init__.py
@@ -1,12 +1,15 @@
 # project/__init__.py
 
+import os
 from flask import Flask, jsonify
 
 # instantiate
 app = Flask(__name__)
 
-app.config.from_object("project.config.DevelopmentConfig")
+app_settings = os.getenv("APP_SETTINGS")
+app.config.from_object(app_settings)
 
+#print(app.config)
 @app.route("/ping", methods=["GET"])
 def ping_pong():
     return jsonify({
